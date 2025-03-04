9267f2c348a27c2dbc60975297f1bba2bf53d897
Bình Hồ <waungbinh@gmail.com>
2025-03-04 21:49:46 +0700

    new home

--- src/app/homepage/home.tsx
+++ src/app/homepage/home.tsx
@@ -44,7 +44,8 @@ const SubContent: React.FC = () => (
 const ContentDivider: React.FC = () => (
     <div className="container mx-auto flex justify-center items-center py-[64px] w-full border-b-[1px] border-red gap-8">
         <h3 className="text-white xl:text-8xl font-thin">Selected Projects</h3>
-        <h3 className="text-accent-500 xl:text-8xl animate-spin-slow">✺</h3>
+        {/* Asterisk google icon */}
+        <h3 className="text-accent-500 xl:text-8xl animate-spin-slow">✺</h3> 
         <h3 className="text-white opacity-50 xl:text-8xl font-thin">2019 - 2024</h3>
     </div>
 );
